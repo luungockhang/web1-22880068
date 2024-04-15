1168694c53a79dd2ae9d0231a061c99b01ac662c
Monkey King <45637966+luungockhang@users.noreply.github.com>
2024-04-15 18:50:02 +0700

    login

--- js/22880068.js
+++ js/22880068.js
@@ -1,4 +1,5 @@
 const API = "https://web1-api.vercel.app/api";
+const AUTHENTICATE_API = "https://web1-api.vercel.app/api/users"
 const siteKey = "6Lea2bcpAAAAAArBqKF1mP8v0lCpOhivhL9JnPZz";
 
 async function loadData(request, templateId, viewId){
@@ -12,3 +13,79 @@ async function loadData(request, templateId, viewId){
     view.innerHTML = template(context);
 };
 
+async function login(e){
+    e.preventDefault();
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    document.getElementById('errorMessage').innerHTML = '';
+
+    try {
+        let token = await getAuthenticateToken(username,password);
+        if (token){
+            localStorage.setItem('token', token);
+            document.getElementsByClassName('btn-close')[0].click(); //có nhiều element có class này nên nói nó là cái đầu tiên [0]
+            displayControls();
+        }
+    } catch(error){
+        document.getElementById('errorMessage').innerHTML = error;
+        displayControls(false);
+    }
+}
+
+async function getAuthenticateToken(username,password){
+    let response = await fetch(`${AUTHENTICATE_API}/authenticate`,{
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json'
+        },
+        body: JSON.stringify({username,password})
+    });
+
+    let result = await response.json();
+    if (response.status = 200){
+        return result.token;
+    }
+    throw new Error(result.message);
+}
+
+function displayControls(isLogin = true){
+    let linkLogins = document.getElementsByClassName('linkLogin');
+    let linkLogouts = document.getElementsByClassName('linkLogout');
+
+    let displayLogin = 'none';
+    let displayLogout = 'block';
+    if (!isLogin){
+        displayLogin = 'block';
+        displayLogout = 'none';
+    }
+
+    for (let i = 0; i < 2; i++){
+        linkLogins[i].style.display = displayLogin;
+        linkLogouts[i].style.display = displayLogout;
+    }
+}
+
+async function checkLogin(){
+    let isLogin = await verifyToken();
+    displayControls(isLogin);
+}
+
+async function verifyToken(){
+    let token = localStorage.getItem('token');
+    if (token){
+        let response = await fetch(`${AUTHENTICATE_API}/verify`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        });
+        if (response.status == 200) {
+            return true;
+        }
+    }
+    return false;
+}
